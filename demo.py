efd19927abb791a0568e3de6dcdc94560758ed66
Josh <josh@usc-eduroam-upc-new40.usc.edu>
2019-07-29 10:30:16 -0700

    Added demo.py

---
+++ demo.py
@@ -0,0 +1,14 @@
+# -------------------------------------------------
+#        Name: Josh
+#        Date: July 29, 2019
+#
+# Description: <include a description
+#               of what the file contains>
+# -------------------------------------------------
+
+def main():
+    name = 'Joshypoo'
+    print('Hell, my name is', name)
+
+if __name__ == '__main__':
+    main()
